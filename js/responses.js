db81d7e344467e36e45e69fb4287f1256e7296c3
tehreenkhan <47290981+tehreenkhan@users.noreply.github.com>
2022-11-29 00:01:05 -0500

    Update responses.js

--- js/responses.js
+++ js/responses.js
@@ -7,25 +7,28 @@ function getBotResponse(input) {
         return "";
     }
 
-    if (input == "Tehreen" ) {
+    if (input == "tehreen" ) {
         return "Please provide your Phone Number.";
     } else if (input == "") {
         return "Incorrect format. Please enter again.";
-    } 
+    } else if (input == "") {
+        return "";
+    }
 
     if (input == "123-456-7890" ) {
         return "Please provide a description of your location.";
     } else if (input == "") {
         return "Incorrect format. Please enter again.";
-    } 
+    } else if (input == "") {
+        return "";
+    }
 
-    // Simple responses
     if (input == "outside of SLC") {
         return "A dispatcher will contact you soon.";
     } else if (input == "ok goodbye") {
         return "Thank you for using WalkSafe assistance. Talk to you later!";
-    } else {
-        return "Try asking something else!";
+    } else if (input == "") {
+        return "";
     }
     
     if (input == "hello") {
